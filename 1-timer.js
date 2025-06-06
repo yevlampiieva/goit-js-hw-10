110c84eadea9abcf7bf0c146908cf21517ce6d35
yevlampiieva <119635848+yevlampiieva@users.noreply.github.com>
2025-06-06 01:22:57 +0000

    Deploying to gh-pages from @ yevlampiieva/goit-js-hw-10@83f5fea784129bcdbfa85d77f246de2d9ebb49dc 🚀

---
+++ 1-timer.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{f as t}from"./assets/vendor-BbbuE1sJ.js";const n=document.querySelector("#datetime-picker");n.addEventListener("input",i);t("#datetime-picker",{enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0])}});function i(){}
+//# sourceMappingURL=1-timer.js.map
